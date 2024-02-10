ab1ee899af4c98693aa1b59f152347e354dbf84f
ayan7001 <134920134+ayan7001@users.noreply.github.com>
2024-02-10 16:51:01 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+document.querySelector("#button-print").addEventListener("click", function () {
+  document.querySelector(".input").style.display = "none";
+  print();
+});
+//var link=document.querySelector("input[name=linkedin]").value;
+//link.slice(link.indexOf("m")+1,link.length)
